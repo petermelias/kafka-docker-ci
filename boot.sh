73f0bfbfb0235e1334373d8a6e460f620e112f6b
Peter M. Elias <petermelias@gmail.com>
2018-05-14 15:25:47 -0500

    docker

--- boot.sh
+++ boot.sh
@@ -3,6 +3,6 @@
 zkServer.sh start
 topics.sh &
 
-printf "listeners=PLAINTEXT://0.0.0.0:$KAFKA_PORT" >> config/server.properties
+printf "listeners=PLAINTEXT://localhost:$KAFKA_PORT" >> config/server.properties
 
 exec kafka-server-start.sh config/server.properties
